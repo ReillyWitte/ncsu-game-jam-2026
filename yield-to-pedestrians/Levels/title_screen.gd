6602e54318ce3f04103736801549ea4ade03f352
Mattacus27 <Mattacus27@gmail.com>
2026-09-19 16:55:27 -0400

    Worked on title screen and game over screen

--- yield-to-pedestrians/Levels/title_screen.gd
+++ yield-to-pedestrians/Levels/title_screen.gd
@@ -5,11 +5,18 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/root_node.tscn")
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	$CenterContainer/MainButtons.visible = false
+	$CenterContainer/SettingsMenu.visible = true
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
-
+	$CenterContainer/MainButtons.visible = false
+	$CenterContainer/CreditsMenu.visible = true
 
 func _on_quit_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	$CenterContainer/MainButtons.visible = true
+	$CenterContainer/SettingsMenu.visible = false
+	$CenterContainer/CreditsMenu.visible = false
