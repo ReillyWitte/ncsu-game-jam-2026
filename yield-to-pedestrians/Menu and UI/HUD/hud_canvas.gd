a95a66380d9c63eb191c350594e9dedd80c0afcd
HamEggMcMuffin <109828368+HamEggMcMuffin@users.noreply.github.com>
2026-09-20 06:38:21 -0400

    audio tweaks, gas warning

--- yield-to-pedestrians/Menu and UI/HUD/hud_canvas.gd
+++ yield-to-pedestrians/Menu and UI/HUD/hud_canvas.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 @onready var gas_needle: Sprite2D = $Needle_Sprite
 @onready var score_label: Label = $Fuel_Gauge_Sprite/Label
 @onready var fuel_light: AnimatedSprite2D = $"Fuel_Gauge_Sprite/Fuel Light"
+@onready var gas_chime: AudioStreamPlayer2D = $"../CharacterBody2D/GameplayMusic/Gas Chime"
+
 
 func _process(delta: float) -> void:
 	gas_needle.rotation = (PI/100) * (Global.fullGasLevel - Global.currentGasLevel)
@@ -10,10 +12,15 @@ func _process(delta: float) -> void:
 	
 	if Global.currentGasLevel/Global.fullGasLevel < 0.20:
 		fuel_light.play("super on")
-		print("super_on")
+		gas_chime.pitch_scale = 2
+		if !gas_chime.playing:
+			gas_chime.play()
 	elif Global.currentGasLevel/Global.fullGasLevel < 0.40:
 		fuel_light.play("on")
-		print("on")
+		gas_chime.pitch_scale = 1
+		if !gas_chime.playing:
+			gas_chime.play()
 	else: 
 		fuel_light.play("off")
-		print("off")
+		gas_chime.stop()
+		
