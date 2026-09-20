4276fd5e6be718468f1e2000c779333285041009
HamEggMcMuffin <109828368+HamEggMcMuffin@users.noreply.github.com>
2026-09-19 23:33:48 -0400

    game over screen tween updates

--- yield-to-pedestrians/Levels/game_over_screen.gd
+++ yield-to-pedestrians/Levels/game_over_screen.gd
@@ -5,6 +5,7 @@ extends CanvasLayer
 @onready var score: Label = $NewsPaper/Score
 
 @onready var color_rect: ColorRect = $ColorRect
+@onready var newspaper: TextureRect = $NewsPaper
 
 
 func _ready():
@@ -18,8 +19,16 @@ func game_end() -> void:
 	get_tree().paused = true
 	self.show()
 	
-	var background_fade = create_tween()
-	background_fade.tween_property(color_rect, "color:a", 1, 1)
+	var ending_animation_tween = create_tween()
+	# Change background
+	ending_animation_tween.tween_property(color_rect, "color:a", 1, 1)
+	
+	# Wait 0.5 seconds
+	ending_animation_tween.tween_interval(0.5)
+	
+	# Rotate and slide in
+	ending_animation_tween.parallel().tween_property(newspaper,"rotation_degrees",-9,1)
+	ending_animation_tween.parallel().tween_property(newspaper,"position",Vector2(400,325),1)
 	
 	
 
