18022c6ca8d693213b79475b4c65e16c57bdcc76
Reilly Witte <reillywitte@gmail.com>
2026-09-20 03:48:40 -0400

    Main menu controller support

--- yield-to-pedestrians/Levels/title_screen.gd
+++ yield-to-pedestrians/Levels/title_screen.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+func _ready() -> void:
+	$CenterContainer/MainButtons/Play.grab_focus()
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/root_node.tscn")
@@ -7,10 +9,13 @@ func _on_play_pressed() -> void:
 func _on_settings_pressed() -> void:
 	$CenterContainer/MainButtons.visible = false
 	$CenterContainer/SettingsMenu.visible = true
+	$CenterContainer/SettingsMenu/HSlider.grab_focus()
 
 func _on_credits_pressed() -> void:
 	$CenterContainer/MainButtons.visible = false
 	$CenterContainer/CreditsMenu.visible = true
+	$CenterContainer/CreditsMenu/Back.grab_focus()
+
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
