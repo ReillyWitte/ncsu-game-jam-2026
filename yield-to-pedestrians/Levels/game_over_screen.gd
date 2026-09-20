0971e8657b146737fa64e7144c0bb51200e21135
HamEggMcMuffin <109828368+HamEggMcMuffin@users.noreply.github.com>
2026-09-19 22:44:58 -0400

    drift to stop and improved game end

--- yield-to-pedestrians/Levels/game_over_screen.gd
+++ yield-to-pedestrians/Levels/game_over_screen.gd
@@ -2,21 +2,24 @@ extends CanvasLayer
 
 @onready var score: Label = $Score
 @onready var dead_num: Label = $DeadNum
+@onready var color_rect: ColorRect = $ColorRect
 
 
 func _ready():
 	self.hide()
 
-func _process(delta: float) -> void:
-	if Global.currentGasLevel <= 0:
-		
-		
-		dead_num.text =str(Global.kill_count) 
-		score.text = str(Global.player_score)
-		
-		get_tree().paused = true
-		self.show()
-
+func game_end() -> void:
+	
+	dead_num.text =str(Global.kill_count) 
+	score.text = str(Global.player_score)
+	
+	get_tree().paused = true
+	self.show()
+	
+	var background_fade = create_tween()
+	background_fade.tween_property(color_rect, "color:a", 1, 1)
+	
+	
 
 
 func _on_retry_pressed() -> void:
